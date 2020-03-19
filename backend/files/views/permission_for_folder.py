c83a18f38323eee8e3b646916894770f4362b2eb
Dominik Walser <jehob@web.de>
2020-03-19 09:59:57 +0100

    doubled name in views removed

--- backend/files/views/permission_for_folder.py
+++ backend/files/views/permission_for_folder.py
@@ -19,6 +19,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-class FolderViewSet(APIView):
-    def get(self, request):
-        pass
+# class FolderViewSet(APIView):
+#     def get(self, request):
+#         pass
