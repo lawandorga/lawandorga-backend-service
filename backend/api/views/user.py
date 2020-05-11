156b43da955e20ab322ea024274caeb1caac9d85
Dominik Walser <jehob@web.de>
2020-05-11 17:03:01 +0200

    login rlc keys not for superuser

--- backend/api/views/user.py
+++ backend/api/views/user.py
@@ -222,8 +222,10 @@ class LoginViewSet(viewsets.ViewSet):
 
             from backend.recordmanagement.helpers import resolve_missing_record_key_entries
             resolve_missing_record_key_entries(token.user, private_key)
-            from backend.api.helpers import resolve_missing_rlc_keys_entries
-            resolve_missing_rlc_keys_entries(token.user, private_key)
+            # TODO: superuser?
+            if not token.user.is_superuser:
+                from backend.api.helpers import resolve_missing_rlc_keys_entries
+                resolve_missing_rlc_keys_entries(token.user, private_key)
 
             return Response(LoginViewSet.get_login_data(token.key, private_key))
         raise CustomError(ERROR__API__LOGIN__INVALID_CREDENTIALS)
