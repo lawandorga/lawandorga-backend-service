7116dc5a5b4680473bd4ae0664a3fb0bafc666b0
Dominik Walser <jehob@web.de>
2020-09-26 14:02:34 +0200

    heroku removed from settings

--- backend/settings.py
+++ backend/settings.py
@@ -29,8 +29,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from datetime import timedelta
 
-import django_heroku
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -194,8 +192,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# heroku and authentication
-django_heroku.settings(locals())
+# authentication
 AUTH_USER_MODEL = "api.UserProfile"
 
 # email
