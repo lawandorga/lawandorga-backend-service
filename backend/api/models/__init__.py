39ae9e94a1e461618d66099f263458d13e6ed66a
Dominik Walser <jehob@web.de>
2020-10-20 16:13:21 +0200

    fixed order of imports

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -30,6 +30,6 @@ from .rlc_settings import *
 from .missing_rlc_keys import *
 from .notification_group import *
 from .notification import *
-from .user_activity import *
 from .user_activity_path import *
+from .user_activity import *
 from .user_session import *
