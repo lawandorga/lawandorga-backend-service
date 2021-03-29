7816dbf325233bf143c943149aa53e49015f7a32
Dominik Walser <jehob@web.de>
2021-03-29 15:34:17 +0200

    manage py reformatted

--- manage.py
+++ manage.py
@@ -19,8 +19,8 @@
 import os
 import sys
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
