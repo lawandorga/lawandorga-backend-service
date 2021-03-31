aeaec8fcb58d32934872350d7f7f53aafe27ab37
Dominik Walser <jehob@web.de>
2021-03-31 10:07:16 +0200

    group permissions for folder fixed

--- backend/files/models/folder.py
+++ backend/files/models/folder.py
@@ -244,31 +244,7 @@ class Folder(ExportModelOperationsMixin("folder"), models.Model):
             return "SEE", relevant_permissions.first()
         return "", None
 
-    def get_all_groups_permissions(self):
-        from backend.api.models.group import Group
-        from backend.files.serializers import PermissionForFolderNestedSerializer
-
-        groups = list(Group.objects.filter(from_rlc=self.rlc))
-
-        allGroupsPermissions = {"SEE": [], "WRITE": [], "READ": []}
-        for group in groups:
-            perm_string, perm = self.get_groups_permission(group)
-            if perm:
-                data = PermissionForFolderNestedSerializer(perm).data
-            else:
-                data = {"general": True}
-            if perm_string == "":
-                continue
-            elif perm_string == "WRITE":
-                allGroupsPermissions["WRITE"].append(data)
-            elif perm_string == "READ":
-                allGroupsPermissions["READ"].append(data)
-            elif perm_string == "SEE":
-                allGroupsPermissions["SEE"].append(data)
-
-        return allGroupsPermissions
-
-    def get_all_groups_permissions_new(
+    def get_all_groups_permissions(
         self,
     ) -> (["PermissionForFolder"], ["PermissionForFolder"], ["HasPermission"]):
         from backend.api.models.group import Group
