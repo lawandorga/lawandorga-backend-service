b8e09ea4429c7c16638ddb92195fb7125458a0ea
danielmoessner <github@moesibert.de>
2021-04-08 19:52:48 +0200

    added middleware again

--- config/settings/base.py
+++ config/settings/base.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # "backend.static.middleware.LoggingMiddleware",
+    "backend.static.middleware.LoggingMiddleware",
 ]
 
 # Url conf
