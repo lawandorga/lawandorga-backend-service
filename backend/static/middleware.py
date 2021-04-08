47b41289a9b4e0bf08021263c0db5bdcf929eeda
danielmoessner <github@moesibert.de>
2021-04-08 19:56:01 +0200

    readded middleware

--- backend/static/middleware.py
+++ backend/static/middleware.py
@@ -33,39 +33,39 @@ class LoggingMiddleware:
         self.get_response = get_response
 
     def __call__(self, request: Request):
-        # if request.path.find("metrics") != -1:
-        #     timeframe = timezone.now() - timedelta(seconds=15)
-        #
-        #     active_users = (
-        #         UserSession.objects.filter(end_time__gt=timeframe)
-        #         .values("user")
-        #         .distinct()
-        #         .count()
-        #     )
-        #     Metrics.currently_active_users.set(active_users)
-        #
-        #     recent_user_sessions = Count(
-        #         "user_sessions", filter=Q(user_sessions__end_time__gt=timeframe)
-        #     )
-        #     rlcs = Rlc.objects.annotate(sessions=recent_user_sessions).values(
-        #         "id", "name", "sessions"
-        #     )
-        #     listed_rlcs = list(rlcs)
-        #
-        #     for rlc in listed_rlcs:
-        #         Metrics.currently_active_users_per_rlc.labels(rlc_name=rlc["name"]).set(
-        #             rlc["sessions"]
-        #         )
+        if request.path.find("metrics") != -1:
+            timeframe = timezone.now() - timedelta(seconds=15)
+
+            active_users = (
+                UserSession.objects.filter(end_time__gt=timeframe)
+                .values("user")
+                .distinct()
+                .count()
+            )
+            Metrics.currently_active_users.set(active_users)
+
+            recent_user_sessions = Count(
+                "user_sessions", filter=Q(user_sessions__end_time__gt=timeframe)
+            )
+            rlcs = Rlc.objects.annotate(sessions=recent_user_sessions).values(
+                "id", "name", "sessions"
+            )
+            listed_rlcs = list(rlcs)
+
+            for rlc in listed_rlcs:
+                Metrics.currently_active_users_per_rlc.labels(rlc_name=rlc["name"]).set(
+                    rlc["sessions"]
+                )
 
         response: Response = self.get_response(request)
 
-        # if (
-        #     request.path.find("unread_notifications") == -1
-        #     and request.path.find("login") == -1
-        #     and request.path.find("user_has_permissions") == -1
-        #     and request.path.find("metrics") == -1
-        # ):
-        #     UserSession.objects.log_user_activity(
-        #         request.user, str(request.path), str(request.method)
-        #     )
+        if (
+            request.path.find("unread_notifications") == -1
+            and request.path.find("login") == -1
+            and request.path.find("user_has_permissions") == -1
+            and request.path.find("metrics") == -1
+        ):
+            UserSession.objects.log_user_activity(
+                request.user, str(request.path), str(request.method)
+            )
         return response
---
+++ fast-deploy.sh
@@ -0,0 +1,2 @@
+git pull
+systemctl restart apache2
