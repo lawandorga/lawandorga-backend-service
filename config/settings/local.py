f59aa00fc27e652930190b155a3b78da3ca24cd3
danielmoessner <github@moesibert.de>
2021-04-08 19:28:35 +0200

    remove middleware stuff

--- config/settings/local.py
+++ config/settings/local.py
@@ -2,7 +2,7 @@ from config.settings.base import *
 
 # Debug
 # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-DEBUG
-DEBUG = True
+DEBUG = False
 
 # Allowed Hosts
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
@@ -36,7 +36,7 @@ DATABASES = {
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "backend.api.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
 }
