44d8a3ee3fc0ace2417e4579336bb3acff185ad2
danielmoessner <github@moesibert.de>
2021-04-08 11:51:55 +0200

    added permissions to deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ git pull
 tmp/venv/bin/pip install -r requirements.txt
 tmp/venv/bin/python manage.py migrate
 tmp/venv/bin/python manage.py collectstatic --clear --noinput
+./permissions.sh
 systemctl restart apache2
