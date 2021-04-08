79293292ff3271f179fb3f85c4392a9cd25fae90
danielmoessner <github@moesibert.de>
2021-04-08 17:04:40 +0200

    improve setup script

--- setup.prod.sh
+++ setup.prod.sh
@@ -7,7 +7,7 @@ touch tmp/logs/django.log
 touch tmp/secrets.json
 # install everything
 apt update
-apt install python3-pip python3-venv python3-dev apache2 libapache2-mod-wsgi-py3
+apt install python3-pip python3-venv python3-dev apache2 libapache2-mod-wsgi-py3 libpq-dev
 snap install core
 snap refresh core
 snap install --classic certbot
@@ -16,4 +16,6 @@ python3 -m venv tmp/venv
 # setup apache configs
 ln -s /home/law-orga-backend/apache.prod.conf /etc/apache2/sites-available/prod-api.law-orga.de.conf
 certbot certonly --apache -d prod-api.law-orga.de --register-unsafely-without-email
+a2enmod ssl
+a2enmod rewrite
 a2ensite prod-api.law-orga.de.conf
