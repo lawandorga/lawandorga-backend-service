9e980d38001edfd035b53d8fd2899ba8cd9c91fa
danielmoessner <github@moesibert.de>
2021-04-29 15:57:01 +0200

    token class changed

--- backend/api/authentication.py
+++ backend/api/authentication.py
@@ -13,13 +13,13 @@
 #
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>
-
+from rest_framework.authtoken.models import Token
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.exceptions import AuthenticationFailed
-from rest_framework.authtoken.models import Token
+from django.conf import settings
+from django.utils import timezone
 from datetime import datetime
 import pytz
-from django.conf import settings
 
 
 class ExpiringTokenAuthentication(TokenAuthentication):
@@ -34,18 +34,11 @@ class ExpiringTokenAuthentication(TokenAuthentication):
         if not token.user.is_active:
             raise AuthenticationFailed("User inactive or deleted")
 
-        utc_now = datetime.utcnow().replace(tzinfo=pytz.timezone(settings.TIME_ZONE))
-
-        if (
-            token.created < (utc_now - settings.TIMEOUT_TIMEDELTA)
-        ) and not token.user.is_superuser:
+        if token.created < (timezone.now() - settings.TIMEOUT_TIMEDELTA):
             token.delete()
             raise AuthenticationFailed("Token has expired")
 
-        token.created = utc_now
-        token.save()
-        token.user.last_login = datetime.utcnow().replace(
-            tzinfo=pytz.timezone(settings.TIME_ZONE)
-        )
+        token.created = timezone.now()
+        token.user.last_login = timezone.now()
         token.user.save()
         return token.user, token
