4b556375736c07c5685a1855eb6eaf91a442f8f7
danielmoessner <github@moesibert.de>
2021-06-11 15:40:33 +0200

    updates

--- backend/api/management/commands/migrate_permissions.py
+++ backend/api/management/commands/migrate_permissions.py
@@ -16,36 +16,8 @@ class Command(BaseCommand):
                 HasPermission.objects.create(**data)
 
     def handle(self, *args, **options):
-        # manage_groups_rlc / manage_group
-        try:
-            permission_to_delete = Permission.objects.get(name=PERMISSION_MANAGE_GROUP)
-            permission_to_replace = Permission.objects.get(name=PERMISSION_MANAGE_GROUPS_RLC)
-            self.replace_permission(permission_to_delete, permission_to_replace)
-            permission_to_delete.delete()
-        except:
-            pass
-        # manage_users / view_full_user_detail_overall && manage_users / view_full_user_detail_own_rlc
-        try:
-            permission_to_replace = Permission.objects.get(name='manage_users')
-            permission_to_delete = Permission.objects.get(name='view_full_user_detail_overall')
-            self.replace_permission(permission_to_delete, permission_to_replace)
-            permission_to_delete.delete()
-            permission_to_delete = Permission.objects.get(name='view_full_user_detail_own_rlc')
-            self.replace_permission(permission_to_delete, permission_to_replace)
-            permission_to_delete.delete()
-        except:
-            pass
-        # manage_groups_rlc / add_group
-        permission_to_delete = Permission.objects.get(name='add_group_rlc')
-        permission_to_replace = Permission.objects.get(name='manage_groups_rlc')
-        self.replace_permission(permission_to_delete, permission_to_replace)
-        permission_to_delete.delete()
-        #
-        try:
-            Permission.objects.get(name='activate_inactive_users').delete()
-        except:
-            pass
-        try:
-            Permission.objects.get(name='process_record_document_deletion_requests').delete()
-        except:
-            pass
+        pass
+        # permission_to_delete = Permission.objects.get(name=PERMISSION_MANAGE_GROUP)
+        # permission_to_replace = Permission.objects.get(name=PERMISSION_MANAGE_GROUPS_RLC)
+        # self.replace_permission(permission_to_delete, permission_to_replace)
+        # permission_to_delete.delete()
