65f04d490eddbfd36de66f3141114c7887c603fb
danielmoessner <github@moesibert.de>
2021-07-04 20:34:22 +0200

    improve deploy script so that it doesnt have to clear staticfiles everytime

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ git reset --hard HEAD
 git pull
 tmp/venv/bin/pip install -r requirements.txt
 tmp/venv/bin/python manage.py migrate
-tmp/venv/bin/python manage.py collectstatic --clear --noinput
+tmp/venv/bin/python manage.py collectstatic --noinput
 ./permissions.sh
 systemctl restart apache2
