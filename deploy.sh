ed932340d18c4ce5b6fded11f2f23dc0e7e1e878
danielmoessner <github@moesibert.de>
2021-10-06 19:08:05 +0200

    add test to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 git reset --hard HEAD
 git pull
 tmp/venv/bin/pip install -r requirements.txt
+tmp/venv/bin/python manage.py test
 tmp/venv/bin/python manage.py migrate
 tmp/venv/bin/python manage.py collectstatic --noinput
 ./permissions.sh
