002a0052dea92e774feda3ecf9194142e0f75f9b
danielmoessner <github@moesibert.de>
2021-10-16 20:26:43 +0200

    remove users_rlc_keys

--- apps/api/views/__init__.py
+++ apps/api/views/__init__.py
@@ -1,18 +1,3 @@
-#  law&orga - record and organization management software for refugee law clinics
-#  Copyright (C) 2019  Dominik Walser
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as
-#  published by the Free Software Foundation, either version 3 of the
-#  License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>
 from .user import *
 from .group import *
 from .permission import *
@@ -21,6 +6,5 @@ from .rlc import *
 from .other import *
 from .language import *
 from .user_encryption_keys import *
-from .users_rlc_keys import *
 from .notification import *
 from .notification_group import *
