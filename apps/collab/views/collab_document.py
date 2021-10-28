263485dd415c25179131ce217fb10cbeb7e56b7c
danielmoessner <github@moesibert.de>
2021-10-28 23:09:52 +0200

    collab api

--- apps/collab/views/collab_document.py
+++ apps/collab/views/collab_document.py
@@ -1,37 +1,14 @@
-#  law&orga - record and organization management software for refugee law clinics
-#  Copyright (C) 2020  Dominik Walser
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as
-#  published by the Free Software Foundation, either version 3 of the
-#  License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>
-
 from typing import Any
-from django.db.models import QuerySet
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.request import Request
-
 from apps.api.errors import CustomError
-from apps.api.models import Group, HasPermission, Permission
+from apps.api.models import HasPermission, Permission
 from apps.api.serializers import OldHasPermissionSerializer
-from apps.collab.models import (
-    CollabDocument,
-    CollabPermission,
-    PermissionForCollabDocument,
-)
+from apps.collab.models import CollabDocument,PermissionForCollabDocument
 from apps.collab.serializers import (
     CollabDocumentSerializer,
-    CollabDocumentTreeSerializer,
     PermissionForCollabDocumentNestedSerializer,
     PermissionForCollabDocumentSerializer, TextDocumentVersionSerializer, CollabDocumentCreateSerializer,
     CollabDocumentListSerializer,
@@ -76,51 +53,8 @@ class CollabDocumentViewSet(viewsets.ModelViewSet):
 
             return [doc for doc in queryset if access(doc)]
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
-    # def list(self, request: Request, **kwargs: Any) -> Response:
-    #     user_has_overall_permission: bool = (
-    #         request.user.has_permission(PERMISSION_READ_ALL_COLLAB_DOCUMENTS_RLC)
-    #         or request.user.has_permission(PERMISSION_WRITE_ALL_COLLAB_DOCUMENTS_RLC)
-    #         or request.user.has_permission(PERMISSION_MANAGE_COLLAB_DOCUMENT_PERMISSIONS_RLC)
-    #     )
-    #     if user_has_overall_permission:
-    #         queryset = self.get_queryset().exclude(path__contains="/").order_by("path")
-    #         data = CollabDocumentTreeSerializer(
-    #             instance=queryset,
-    #             user=request.user,
-    #             all_documents=self.get_queryset().order_by("path"),
-    #             overall_permission=user_has_overall_permission,
-    #             see_subfolders=False,
-    #             many=True,
-    #             context={request: request},
-    #         ).data
-    #     else:
-    #         queryset = self.get_queryset().exclude(path__contains="/").order_by("path")
-    #         data = []
-    #         for document in queryset:
-    #             see, direct = document.user_can_see(request.user)
-    #             if see:
-    #                 data.append(
-    #                     CollabDocumentTreeSerializer(
-    #                         instance=document,
-    #                         user=request.user,
-    #                         all_documents=self.get_queryset().order_by("path"),
-    #                         overall_permission=user_has_overall_permission,
-    #                         many=False,
-    #                         see_subfolders=direct,
-    #                         context={request: request},
-    #                     ).data
-    #                 )
-    #     return Response(data)
-
     def destroy(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        if not CollabDocument.user_has_permission_write(
-            self.get_object().path, request.user
-        ):
+        if not CollabDocument.user_has_permission_write(self.get_object().path, request.user):
             raise CustomError(ERROR__API__PERMISSION__INSUFFICIENT)
         return super().destroy(request, *args, **kwargs)
 
