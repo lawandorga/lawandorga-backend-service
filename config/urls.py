b3c90ee8d81240da6e9e6688c2197e0371d363c0
danielmoessner <github@moesibert.de>
2021-11-26 20:05:34 +0100

    add error path for testing

--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,3 @@
-#  law&orga - record and organization management software for refugee law clinics
-#  Copyright (C) 2019  Dominik Walser
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as
-#  published by the Free Software Foundation, either version 3 of the
-#  License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>
 from apps.recordmanagement import urls as record_urls
 from django.views.generic import TemplateView
 from django.contrib import admin
@@ -22,6 +7,7 @@ from apps.api import urls as api_urls
 
 
 urlpatterns = [
+    path('error/', TemplateView.as_view(template_name='')),
     path("admin/", admin.site.urls),
     path("api/", include(api_urls)),
     path("api/records/", include(record_urls)),
