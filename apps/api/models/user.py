1b8debe99e7103b00a979e0fd808c9e6e2ff75e5
danielmoessner <github@moesibert.de>
2021-12-25 17:22:28 +0100

    change test stuff

--- apps/api/models/user.py
+++ apps/api/models/user.py
@@ -155,11 +155,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
             private_key = request.META.get("HTTP_PRIVATE_KEY")
             if not private_key:
                 # enable direct testing of the rest framework
-                if (
-                    settings.DEBUG
-                    and self.email == "dummy@law-orga.de"
-                    and settings.DUMMY_USER_PASSWORD
-                ):
+                if self.email == "dummy@law-orga.de" and settings.DUMMY_USER_PASSWORD:
                     return self.encryption_keys.decrypt_private_key(settings.DUMMY_USER_PASSWORD)
                 else:
                     raise ParseError('No private key provied within the request.')
