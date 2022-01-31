e499a36261d564f5c55cdb7513e31e9cc12e9836
Daniel Mössner <github@tortuga-webdesign.de>
2022-01-31 20:50:00 +0100

    collab

--- apps/collab/models/collab_document.py
+++ apps/collab/models/collab_document.py
@@ -1,3 +1,4 @@
+from apps.api.models import Rlc, UserProfile
 from apps.collab.static.collab_permissions import PERMISSION_WRITE_DOCUMENT
 from apps.static.permissions import PERMISSION_READ_ALL_COLLAB_DOCUMENTS_RLC, PERMISSION_WRITE_ALL_COLLAB_DOCUMENTS_RLC
 from apps.collab.models import TextDocument
@@ -6,6 +7,11 @@ from typing import Dict, Tuple
 
 
 class CollabDocument(TextDocument):
+    # rlc = models.ForeignKey(Rlc, related_name="text_documents", null=True, on_delete=models.CASCADE, blank=True)
+    # created = models.DateTimeField(auto_created=True)
+    # updated = models.DateTimeField(auto_now=True)
+    # creator = models.ForeignKey(UserProfile, related_name="text_documents_created", on_delete=models.SET_NULL,
+    #                             null=True, blank=True)
     path = models.CharField(max_length=4096, null=False, blank=False)
 
     class Meta:
