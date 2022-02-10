78122496016d1bb2a53db5260ed20e3561b6917b
Daniel Mössner <github@tortuga-webdesign.de>
2022-02-10 21:36:02 +0100

    add description to permission

--- apps/api/models/permission.py
+++ apps/api/models/permission.py
@@ -1,23 +1,9 @@
-#  law&orga - record and organization management software for refugee law clinics
-#  Copyright (C) 2019  Dominik Walser
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as
-#  published by the Free Software Foundation, either version 3 of the
-#  License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>
 from django.db import models
 
 
 class Permission(models.Model):
     name = models.CharField(max_length=200, null=False, unique=True)
+    description = models.TextField(blank=True)
 
     class Meta:
         verbose_name = "Permission"
@@ -25,25 +11,3 @@ class Permission(models.Model):
 
     def __str__(self):
         return "permission: {}; name: {};".format(self.pk, self.name)
-
-    def get_groups_with_permission_from_rlc(self, rlc):
-        from apps.api.models import Group, HasPermission
-
-        groups = Group.objects.filter(from_rlc=rlc)
-        return HasPermission.objects.filter(
-            permission=self,
-            group_has_permission__in=groups.values_list("pk", flat=True),
-        )
-
-    def get_users_with_permission_from_rlc(self, rlc):
-        """
-        TODO: rename this method, return not users but HasPermissions
-        :param rlc:
-        :return:
-        """
-        from apps.api.models import UserProfile, HasPermission
-
-        users = UserProfile.objects.filter(rlc=rlc)
-        return HasPermission.objects.filter(
-            permission=self, user_has_permission__in=users.values_list("pk", flat=True)
-        )
--- apps/collab/views/collab_document.py
+++ apps/collab/views/collab_document.py
@@ -1,15 +1,14 @@
 from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied
-from apps.collab.serializers import CollabDocumentSerializer, \
-    TextDocumentVersionSerializer, CollabDocumentCreateSerializer, \
-    CollabDocumentListSerializer, TextDocumentVersionCreateSerializer, PermissionForCollabDocumentAllNamesSerializer, \
+from apps.collab.serializers import CollabDocumentSerializer, TextDocumentVersionSerializer, \
+    CollabDocumentCreateSerializer, CollabDocumentListSerializer, PermissionForCollabDocumentAllNamesSerializer, \
     CollabDocumentRetrieveSerializer, CollabDocumentUpdateSerializer
-from apps.api.static import PERMISSION_MANAGE_COLLAB_DOCUMENT_PERMISSIONS_RLC, \
-    PERMISSION_READ_ALL_COLLAB_DOCUMENTS_RLC, PERMISSION_WRITE_ALL_COLLAB_DOCUMENTS_RLC
 from rest_framework.response import Response
 from rest_framework.request import Request
-from apps.collab.models import CollabDocument, PermissionForCollabDocument, TextDocumentVersion
-from rest_framework import viewsets, status, mixins
+from apps.collab.models import CollabDocument, PermissionForCollabDocument
+from apps.api.static import PERMISSION_MANAGE_COLLAB_DOCUMENT_PERMISSIONS_RLC, \
+    PERMISSION_READ_ALL_COLLAB_DOCUMENTS_RLC, PERMISSION_WRITE_ALL_COLLAB_DOCUMENTS_RLC
+from rest_framework import viewsets, mixins
 from typing import Any
 
 
