dc8f770345a69a88203be9e581d615efcd938006
Daniel Mössner <github@tortuga-webdesign.de>
2022-02-10 22:06:11 +0100

    add recommended for on permission

--- apps/api/models/permission.py
+++ apps/api/models/permission.py
@@ -4,6 +4,7 @@ from django.db import models
 class Permission(models.Model):
     name = models.CharField(max_length=200, null=False, unique=True)
     description = models.TextField(blank=True)
+    recommended_for = models.CharField(max_length=200)
 
     class Meta:
         verbose_name = "Permission"
