ef31ddbaa330b44e94432a8efad7ef8f03d4709f
Daniel Mössner <github@tortuga-webdesign.de>
2022-03-02 18:55:20 +0100

    record ordering

--- apps/recordmanagement/models/record.py
+++ apps/recordmanagement/models/record.py
@@ -223,6 +223,7 @@ class Record(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
+        ordering = ['-created']
         verbose_name = 'Record'
         verbose_name_plural = 'Records'
 
