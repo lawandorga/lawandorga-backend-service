877aee0bc6e2ae07641ad2a05914085bdf54efd2
Daniel Mössner <github@tortuga-webdesign.de>
2022-04-13 12:45:04 +0200

    more statistic

--- apps/api/views/__init__.py
+++ apps/api/views/__init__.py
@@ -2,7 +2,6 @@ from .rlc_user import *
 from .group import *
 from .has_permission import *
 from .rlc import *
-from .other import *
 from .notification import *
 from .notification_group import *
 from .permissions import *
--- apps/api/views/statistic.py
+++ apps/api/views/statistic.py
@@ -1,9 +1,8 @@
-from rest_framework.decorators import action
 from rest_framework.permissions import BasePermission
+from rest_framework.decorators import action
 from rest_framework.response import Response
-
-from apps.api.models import LoggedPath
 from config.authentication import IsAuthenticatedAndEverything
+from apps.api.models import LoggedPath
 from rest_framework import viewsets
 from django.db import connection
 
@@ -35,4 +34,45 @@ class StatisticViewSet(viewsets.GenericViewSet):
         """
         data = self.execute_statement(statement)
         data = map(lambda x: {'name': x[0], 'amount': x[1]}, data)
+        return Response(data)
+
+    @action(detail=False)
+    def record_states(self, request, *args, **kwargs):
+        statement = """
+        select state, count(amount) as amount
+        from (
+            select count(state.record_id) as amount,
+                state.record_id,
+                case
+                    when count(state.record_id) <> 1 or state.value = '' or state.value is null then 'Unknown'
+                    else state.value
+                end as state
+            from recordmanagement_record as record
+            left join recordmanagement_recordstateentry as state on state.record_id = record.id
+            group by record.id, state.record_id, state.value
+        ) as tmp
+        group by state
+        """
+        data = self.execute_statement(statement)
+        data = map(lambda x: {'state': x[0], 'amount': x[1]}, data)
+        return Response(data)
+
+    @action(detail=False)
+    def lc_usage(self, request, *args, **kwargs):
+        statement = """
+        select
+            rlc.name as name,
+            count(distinct record.id) as records,
+            count(distinct file.id) as files,
+            count(distinct document.id) as documents
+        from api_rlc as rlc
+        left join recordmanagement_recordtemplate template on rlc.id = template.rlc_id
+        left join recordmanagement_record record on record.template_id = template.id
+        left join files_folder folder on rlc.id = folder.rlc_id
+        left join files_file file on file.folder_id = folder.id
+        left join collab_collabdocument document on rlc.id = document.rlc_id
+        group by rlc.id, rlc.name;
+        """
+        data = self.execute_statement(statement)
+        data = map(lambda x: {'lc': x[0], 'records': x[1], 'files': x[2], 'documents': x[3]}, data)
         return Response(data)
