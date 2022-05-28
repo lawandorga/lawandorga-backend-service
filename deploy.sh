b6b1f2f9039bb97623c8b6e927ba7bead734d8f6
Daniel Mössner <github@tortuga-webdesign.de>
2022-05-28 22:04:25 +0200

    improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,8 @@ cd /home/law-orga-backend/ || exit
 git reset --hard HEAD
 git pull
 tmp/venv/bin/pip install -r requirements.txt
-tmp/venv/bin/python production_manage.py test
 tmp/venv/bin/python production_manage.py migrate
-tmp/venv/bin/python production_manage.py collectstatic --noinput
+#tmp/venv/bin/python production_manage.py collectstatic --noinput
 ./permissions.sh
 systemctl restart apache2
 echo 'DEPLOYED'
