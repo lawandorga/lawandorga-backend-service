e3d25f262db60a15c31e24a28cf2202dec51eaf1
Daniel Mössner <github@tortuga-webdesign.de>
2022-07-26 17:20:36 +0200

    fix tests

--- apps/api/tests/example_data.py
+++ apps/api/tests/example_data.py
@@ -13,7 +13,7 @@ from apps.collab.models import CollabDocument, CollabPermission, TextDocumentVer
 from apps.collab.static import get_all_collab_permission_strings
 from apps.files.models import FolderPermission
 from apps.files.static import get_all_files_permission_strings
-from apps.internal.models import InternalUser
+from apps.api.models import InternalUser
 from apps.recordmanagement.fixtures import create_default_record_template
 from apps.recordmanagement.models import (
     QuestionnaireQuestion,
