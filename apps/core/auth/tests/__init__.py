e332e9fafc2b0dcfa0b70616b466fb7b4033a47b
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2022-09-17 17:24:21 +0200

    precommit

--- apps/core/auth/tests/__init__.py
+++ apps/core/auth/tests/__init__.py
@@ -1,6 +1,6 @@
 from .data import *
+from .test_keys import *
 from .test_rlc_user import *
 from .test_rlc_user_2 import *
 from .test_statistic_user import *
-from .test_keys import *
 from .test_user_unit import *
--- apps/core/rlc/tests/__init__.py
+++ apps/core/rlc/tests/__init__.py
@@ -1,4 +1,4 @@
 from .example_data import *
-from .test_note import *
 from .test_example_data import *
+from .test_note import *
 from .test_user_encryption_keys import *
