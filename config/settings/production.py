02d21ff6f8a01966dc0d20fb8481fca98c5e0058
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2022-09-22 22:14:38 +0200

    fix wrong settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -38,10 +38,10 @@ DATABASES = {
 # https://docs.djangoproject.com/en/dev/topics/email/#smtp-backend
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = env.str("EMAIL_HOST")
-DEFAULT_FROM_EMAIL = env.str("EMAIL_HOST")
-SERVER_EMAIL = env.str("EMAIL_HOST")
+DEFAULT_FROM_EMAIL = env.str("EMAIL_ADDRESS")
+SERVER_EMAIL = env.str("EMAIL_ADDRESS")
 EMAIL_PORT = env.int("EMAIL_PORT")
-EMAIL_HOST_USER = env.str("EMAIL_HOST")
+EMAIL_HOST_USER = env.str("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = env.str("EMAIL_PASSWORD")
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
