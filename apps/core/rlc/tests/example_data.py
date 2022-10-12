ea7bca1200ae35586f60b3985bf8a19bafedf515
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2022-10-12 14:32:03 +0200

    precommit and fix tests

--- apps/core/rlc/tests/example_data.py
+++ apps/core/rlc/tests/example_data.py
@@ -703,7 +703,7 @@ def create_informative_record(main_user, main_user_password, users, rlc):
         created="2019-3-11T10:12:21+00:00",
         message="Bitte dringend die Kontaktdaten des Mandanten eintragen.",
     )
-    message1.encrypt(main_user, main_user.get_private_key(main_user_password))
+    message1.encrypt(main_user.rlc_user, main_user.get_private_key(main_user_password))
     message1.save()
     message2 = EncryptedRecordMessage(
         sender=choice(users).rlc_user,
@@ -711,7 +711,7 @@ def create_informative_record(main_user, main_user_password, users, rlc):
         created="2019-3-12T9:32:21",
         message="Ist erledigt! Koennen wir uns morgen treffen um das zu besprechen?",
     )
-    message2.encrypt(main_user, main_user.get_private_key(main_user_password))
+    message2.encrypt(main_user.rlc_user, main_user.get_private_key(main_user_password))
     message2.save()
     message3 = EncryptedRecordMessage(
         sender=main_user.rlc_user,
@@ -719,7 +719,7 @@ def create_informative_record(main_user, main_user_password, users, rlc):
         created="2019-3-12T14:7:21",
         message="Klar, einfach direkt in der Mittagspause in der Mensa.",
     )
-    message3.encrypt(main_user, main_user.get_private_key(main_user_password))
+    message3.encrypt(main_user.rlc_user, main_user.get_private_key(main_user_password))
     message3.save()
     message4 = EncryptedRecordMessage(
         sender=choice(users).rlc_user,
@@ -727,7 +727,7 @@ def create_informative_record(main_user, main_user_password, users, rlc):
         created="2019-3-13T18:7:21",
         message="Gut, jetzt faellt mir aber auch nichts mehr ein.",
     )
-    message4.encrypt(main_user, main_user.get_private_key(main_user_password))
+    message4.encrypt(main_user.rlc_user, main_user.get_private_key(main_user_password))
     message4.save()
 
     # return
