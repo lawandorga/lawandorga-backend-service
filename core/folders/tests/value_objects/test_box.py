2f1b5261057d560eb539aed041fbecbe598733b9
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2022-10-25 21:30:23 +0200

    fix tests

--- core/folders/tests/value_objects/test_box.py
+++ core/folders/tests/value_objects/test_box.py
@@ -1,16 +1,16 @@
 import pytest
 
 from core.folders.domain.value_objects.box import LockedBox, OpenBox
+from core.folders.domain.value_objects.encryption import EncryptionPyramid
 from core.folders.domain.value_objects.keys import SymmetricKey
-from core.folders.tests.helpers.encryptions import (
-    SymmetricEncryptionTest1,
-    SymmetricEncryptionTest2,
-)
+from core.folders.tests.helpers.encryptions import SymmetricEncryptionTest1
 
 
 class TestKey(SymmetricKey):
-    def __init__(self, key: str):
+    def __init__(self, key: str, origin: str):
         self.__key = key
+        self._origin = origin
+        super().__init__()
 
     def get_key(self) -> str:
         return self.__key
@@ -18,16 +18,19 @@ class TestKey(SymmetricKey):
 
 @pytest.fixture
 def key():
-    yield TestKey(SymmetricEncryptionTest1.generate_key())
+    EncryptionPyramid.reset_encryption_hierarchies()
+    EncryptionPyramid.add_symmetric_encryption(SymmetricEncryptionTest1)
+    key, version = SymmetricEncryptionTest1.generate_key()
+    yield TestKey(key, version)
 
 
 def test_box_is_bytes(key):
     o1 = OpenBox(data=b"Secret")
     assert o1 == b"Secret"
     assert isinstance(o1, bytes)
-    l1 = o1.lock(key, encryption_class=SymmetricEncryptionTest1)
+    l1 = key.lock(o1)
     assert isinstance(l1, bytes)
-    o2 = l1.unlock(key)
+    o2 = key.unlock(l1)
     assert o2 == b"Secret"
     assert isinstance(o2, bytes)
 
@@ -35,18 +38,18 @@ def test_box_is_bytes(key):
 def test_box_can_be_unloaded(key):
     o1 = OpenBox(data=b"Secret")
     assert o1 == b"Secret"
-    l1 = o1.lock(key, encryption_class=SymmetricEncryptionTest1)
+    l1 = key.lock(o1)
     enc_data = l1.value
-    l2 = LockedBox(enc_data=enc_data, encryption_class=SymmetricEncryptionTest1)
+    l2 = LockedBox(enc_data=enc_data, encryption_version=key.origin)
     assert enc_data == l2
-    o2 = l2.unlock(key)
+    o2 = key.unlock(l2)
     assert o2 == b"Secret"
 
 
 def test_box_decryption_fails_with_another_encryption_class(key):
     o1 = OpenBox(data=b"Secret")
-    l1 = o1.lock(key, encryption_class=SymmetricEncryptionTest1)
+    l1 = key.lock(o1)
     enc_data = l1.value
-    l2 = LockedBox(enc_data=enc_data, encryption_class=SymmetricEncryptionTest2)
-    with pytest.raises(KeyError):
-        l2.unlock(key)
+    l2 = LockedBox(enc_data=enc_data, encryption_version="ST0")
+    with pytest.raises(ValueError):
+        key.unlock(l2)
