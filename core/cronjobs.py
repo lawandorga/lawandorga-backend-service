c5d44389e22be8341503839f8c1c2c0f160a86e0
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2022-10-31 14:13:17 +0100

    add cronjobs schedule

--- core/cronjobs.py
+++ core/cronjobs.py
@@ -21,7 +21,7 @@ class CronjobWarehouse:
                 module_name = ".".join(s.split(".")[:-1])
                 module = importlib.import_module(module_name)
                 function_name = s.split(".")[-1]
-                job = getattr(module, function_name)
+                job: Callable[[], str] = getattr(module, function_name)
                 cls.add_cronjob(job)
 
     @classmethod
@@ -29,30 +29,30 @@ class CronjobWarehouse:
         cls.__CRONJOBS.append(job)
 
     @classmethod
-    async def run_cronjobs(cls):
+    def run_cronjobs(cls):
         cls.setup_cronjobs()
         results = []
         for job in cls.__CRONJOBS:
             try:
-                result = await sync_to_async(job, thread_sensitive=False)()
+                result = job()
             except Exception as e:
                 result = "ERROR {}".format(str(e))
-            results.append(result)
+            status = '{}: {}'.format(job.__code__.co_name, result)
+            results.append(status)
         cls.__RUNS[timezone.now().strftime("%Y-%m-%d---%H:%M:%S")] = results
+        return results
 
     @classmethod
-    def status(cls):
+    def get_runs(cls):
         return cls.__RUNS
 
 
 @router.api(url="run/", output_schema=dict)
 async def run_cronjobs():
-    # loop = asyncio.get_event_loop()
-    # loop.create_task(CronjobWarehouse.run_cronjobs())
-    await CronjobWarehouse.run_cronjobs()
+    await sync_to_async(CronjobWarehouse.run_cronjobs)()
     return {"status": "OK"}
 
 
 @router.api(url="status/", output_schema=dict[str, list[str]])
-def status():
-    return CronjobWarehouse.status()
+def get_status():
+    return CronjobWarehouse.get_runs()
---
+++ core/management/commands/cronjobs.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+
+from core.cronjobs import CronjobWarehouse
+
+
+class Command(BaseCommand):
+    help = "Runs cronjobs."
+
+    def handle(self, *args, **options):
+        results = CronjobWarehouse.run_cronjobs()
+        self.stdout.write('\n'.join(results))
