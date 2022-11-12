0dd2d17601ee7709d3a87a42ad8136cd4b587a35
Frederico Bormann <github@frederico.info>
2022-11-12 12:44:26 +0100

    Add endpoint to reset calendar ics url

--- core/events/api/ics.py
+++ core/events/api/ics.py
@@ -21,3 +21,9 @@ def get_ics_calendar(request, calendar_uuid: uuid.UUID):
 @router.get(output_schema=CalendarUuidUser)
 def get_calender_uuid(rlc_user: RlcUser):
     return {"id": rlc_user.id, "calendar_uuid": rlc_user.calendar_uuid, "calendar_url": f"{settings.CALENDAR_URL}{rlc_user.calendar_uuid}.ics"}
+
+
+@router.post(url="reset/", output_schema=CalendarUuidUser)
+def reset_calendar_uuid(rlc_user: RlcUser):
+    rlc_user.regenerate_calendar_uuid()
+    return {"id": rlc_user.id, "calendar_uuid": rlc_user.calendar_uuid, "calendar_url": f"{settings.CALENDAR_URL}{rlc_user.calendar_uuid}.ics"}
