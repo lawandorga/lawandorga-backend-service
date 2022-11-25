08a4a99776e63ddc41a1fa77d4a2673a7342cccf
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2022-11-25 10:43:02 +0100

    a

--- core/apps.py
+++ core/apps.py
@@ -3,3 +3,18 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = "core"
+
+    def ready(self) -> None:
+        from core.folders.domain.value_objects.encryption import EncryptionWarehouse
+        from core.folders.infrastructure.asymmetric_encryptions import (
+            AsymmetricEncryptionV1,
+        )
+        from core.folders.infrastructure.folder_repository import DjangoFolderRepository
+        from core.folders.infrastructure.symmetric_encryptions import (
+            SymmetricEncryptionV1,
+        )
+        from core.seedwork.repository import RepositoryWarehouse
+
+        EncryptionWarehouse.add_asymmetric_encryption(AsymmetricEncryptionV1)
+        EncryptionWarehouse.add_symmetric_encryption(SymmetricEncryptionV1)
+        RepositoryWarehouse.add_repository(DjangoFolderRepository)
