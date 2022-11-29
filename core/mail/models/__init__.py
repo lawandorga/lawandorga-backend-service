708d9e03a561c8d63ec860e6c634227f169ea366
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2022-11-29 14:06:02 +0100

    add mail admin

--- core/mail/models/__init__.py
+++ core/mail/models/__init__.py
@@ -1,5 +1,6 @@
 from .account import MailAccount
 from .address import MailAddress
+from .admin import MailAdmin
 from .domain import MailDomain
 from .org import MailOrg
 from .user import MailUser
---
+++ core/mail/models/admin.py
@@ -0,0 +1,19 @@
+from uuid import uuid4
+
+from django.db import models
+
+from core.mail.models.user import MailUser
+
+
+class MailAdmin(models.Model):
+    uuid = models.UUIDField(db_index=True, default=uuid4)
+    user = models.OneToOneField(
+        MailUser, related_name="admin", on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = "MailAdmin"
+        verbose_name_plural = "MailAdmins"
+
+    def __str__(self):
+        return "mailAdmin: {};".format(self.id)
