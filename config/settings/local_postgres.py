967158d8efc64df746351520ff181726dac7b37e
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2022-12-08 18:25:04 +0100

    add local postgres option

---
+++ config/settings/local_postgres.py
@@ -0,0 +1,16 @@
+from .local import *
+
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "lawandorga-backend-service",
+        "USER": "lawandorga-backend-service",
+        "PASSWORD": "pass1234",
+        "HOST": "localhost",
+        "PORT": 5432,
+        "TEST": {"NAME": "test_{}".format(RUNTIME)},
+    }
+}
