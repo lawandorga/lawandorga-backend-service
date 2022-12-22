4238430d2583e7a818e4cd6233d3111ed62e2f4c
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2022-12-22 12:47:21 +0100

    deploy hard

--- core/files_new/api/file.py
+++ core/files_new/api/file.py
@@ -11,10 +11,12 @@ router = Router()
 
 @router.post(input_schema=schemas.InputUploadFile)
 def command__upload_file(rlc_user: RlcUser, data: schemas.InputUploadFile):
-    if not isinstance(data.file, InMemoryUploadedFile):
-        raise ApiError({"file": ["You need to submit a file"]})
+    raise ApiError('File creation is disabled at the moment. It will be enabled again at about 2pm 22. Dec 2022.')
 
-    upload_a_file(rlc_user, data.file, data.folder)
+    # if not isinstance(data.file, InMemoryUploadedFile):
+    #     raise ApiError({"file": ["You need to submit a file"]})
+    #
+    # upload_a_file(rlc_user, data.file, data.folder)
 
 
 @router.delete(url="<uuid:uuid>/", input_schema=schemas.InputDeleteFile)
