11428b237a6cfe23d897d035245c10be5c45ff9e
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2022-12-26 15:44:34 +0100

    add test

--- core/records/tests/api/test_query.py
+++ core/records/tests/api/test_query.py
@@ -6,3 +6,10 @@ def test_records_page(user, record):
     c.login(**user)
     response = c.get("/api/records/query/")
     assert response.status_code == 200
+
+
+def test_record_detail(user, record):
+    c = Client()
+    c.login(**user)
+    response = c.get("/api/records/query/{}/".format(record.uuid))
+    assert response.status_code == 200
