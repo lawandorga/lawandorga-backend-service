5b00f81c40674f18777128ce763ae1935a60ca11
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2023-01-04 13:58:59 +0100

    fix test

--- core/other/deprecated/tests/conftest.py
+++ core/other/deprecated/tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 
 from core.folders.domain.aggregates.folder import Folder
 from core.folders.domain.value_objects.encryption import EncryptionWarehouse
+from core.folders.infrastructure.asymmetric_encryptions import AsymmetricEncryptionV1
+from core.folders.infrastructure.symmetric_encryptions import SymmetricEncryptionV1
 from core.folders.tests.helpers.car import CarWithSecretName
 from core.folders.tests.helpers.encryptions import (
     AsymmetricEncryptionTest1,
@@ -12,14 +14,13 @@ from core.folders.tests.helpers.encryptions import (
 from core.folders.tests.helpers.user import UserObject
 from core.other.deprecated.content_upgrade import Content, ContentUpgrade
 
-# from core.folders.infrastructure.asymmetric_encryptions import AsymmetricEncryptionV1
-# from core.folders.infrastructure.symmetric_encryptions import SymmetricEncryptionV1
-
 
 @pytest.fixture
 def encryption_reset():
     EncryptionWarehouse.reset_encryption_hierarchies()
     yield
+    EncryptionWarehouse.add_asymmetric_encryption(AsymmetricEncryptionV1)
+    EncryptionWarehouse.add_symmetric_encryption(SymmetricEncryptionV1)
 
 
 @pytest.fixture
