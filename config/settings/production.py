761f01d5645f9136a2fa85b13b02010b765886cb
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2023-01-07 15:33:26 +0100

    deploy with sentry logging

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,4 +1,6 @@
 import environs
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 from .base import *
 
@@ -130,3 +132,20 @@ LOGGING = {
 
 # This is used for ics calendar integration links
 CALENDAR_URL = "https://calendar.law-orga.de/api/events/ics/"
+
+
+# sentry
+# see: https://sentry.law-orga.de/sentry/lawandorga-backend-service/getting-started/python-django/
+sentry_sdk.init(
+    dsn=env.str("SENTRY_DSN"),
+    integrations=[
+        DjangoIntegration(),
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
