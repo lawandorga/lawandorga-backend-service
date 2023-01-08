32e0c8f35cad1f60cd41c172d770a191ea269951
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2023-01-08 16:49:06 +0100

    add an environment

--- config/settings/production.py
+++ config/settings/production.py
@@ -153,4 +153,5 @@ sentry_sdk.init(
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
     send_default_pii=True,
+    environment='production',
 )
