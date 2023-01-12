2d6b57a1c30b9aad167e69e13b66f1157aa7227c
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2023-01-12 19:27:59 +0100

    reduce code smells

--- config/handlers.py
+++ config/handlers.py
@@ -3,21 +3,21 @@ from django.http import JsonResponse
 from core.seedwork.api_layer import RFC7807
 
 
-def handler400(request, exception, template_name="none.html"):
+def handler400(_, __, ___=""):
     error = RFC7807(err_type="DjangoBadRequest", title="Bad Request", status=400)
     return JsonResponse(error.dict(), status=400)
 
 
-def handler403(request, exception, template_name="none.html"):
+def handler403(_, __, ___=""):
     error = RFC7807(err_type="DjangoForbidden", title="Forbidden", status=403)
     return JsonResponse(error.dict(), status=403)
 
 
-def handler404(request, exception, template_name="none.html"):
+def handler404(_, __, ___=""):
     error = RFC7807(err_type="DjangoNotFound", title="Not Found", status=404)
     return JsonResponse(error.dict(), status=404)
 
 
-def handler500(request, template_name="none.html"):
+def handler500(_, __=""):
     error = RFC7807(err_type="DjangoServerError", title="Server Error", status=500)
     return JsonResponse(error.dict(), status=500)
