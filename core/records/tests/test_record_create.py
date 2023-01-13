f5b9de3d4aa02b107feb8267028d4018a4f8ce45
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2023-01-13 13:06:27 +0100

    fix stuff

--- core/records/tests/test_record_create.py
+++ core/records/tests/test_record_create.py
@@ -17,8 +17,8 @@ def test_create_within_folder(user, record_template, folder):
     create_a_record_within_a_folder(
         user["rlc_user"],
         "record123",
-        folder=folder.uuid,
-        template=record_template["template"].pk,
+        folder_uuid=folder.uuid,
+        template_id=record_template["template"].pk,
     )
 
 
@@ -27,7 +27,7 @@ def test_create_record_and_folder(user, record_template, folder):
     create_a_record_and_a_folder(
         user["rlc_user"],
         name="My folder that contains a record",
-        template=record_template["template"].pk,
+        template_id=record_template["template"].pk,
     )
 
 
@@ -36,8 +36,8 @@ def test_create_no_permission(user, record_template, folder):
         create_a_record_within_a_folder(
             user["rlc_user"],
             "record123",
-            folder=folder.uuid,
-            template=record_template["template"].pk,
+            folder_uuid=folder.uuid,
+            template_id=record_template["template"].pk,
         )
 
 
@@ -46,14 +46,14 @@ def test_records_are_added_to_folder(user, record_template, folder, folder_repo)
     create_a_record_within_a_folder(
         user["rlc_user"],
         "record123",
-        folder=folder.uuid,
-        template=record_template["template"].pk,
+        folder_uuid=folder.uuid,
+        template_id=record_template["template"].pk,
     )
     create_a_record_within_a_folder(
         user["rlc_user"],
         "record123",
-        folder=folder.uuid,
-        template=record_template["template"].pk,
+        folder_uuid=folder.uuid,
+        template_id=record_template["template"].pk,
     )
     f = folder_repo.retrieve(folder.org_pk, folder.uuid)
     assert 2 == len(f.items)
@@ -66,8 +66,8 @@ def test_no_access_to_folder(user, record_template, folder, another_user):
         create_a_record_within_a_folder(
             another_user["rlc_user"],
             "record123",
-            folder=folder.uuid,
-            template=record_template["template"].pk,
+            folder_uuid=folder.uuid,
+            template_id=record_template["template"].pk,
         )
 
 
@@ -84,8 +84,8 @@ def test_grant_to_users_with_general_permission(
     record_id = create_a_record_within_a_folder(
         user["rlc_user"],
         "record123",
-        folder=folder.uuid,
-        template=record_template["template"].pk,
+        folder_uuid=folder.uuid,
+        template_id=record_template["template"].pk,
     )
     folder_uuid = Record.objects.get(id=record_id).folder_uuid
 
@@ -107,7 +107,7 @@ def test_grant_to_users_with_general_permission_two(
     record_id = create_a_record_and_a_folder(
         user["rlc_user"],
         "record123",
-        template=record_template["template"].pk,
+        template_id=record_template["template"].pk,
     )
     folder_uuid = Record.objects.get(id=record_id).folder_uuid
 
