1bcaaebbc956caff26daa257d2f736b807a869d6
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2023-02-27 02:06:04 +0100

    fix error

--- core/rlc/use_cases/note.py
+++ core/rlc/use_cases/note.py
@@ -6,21 +6,21 @@ from core.static import PERMISSION_DASHBOARD_MANAGE_NOTES
 
 
 @use_case(permissions=[PERMISSION_DASHBOARD_MANAGE_NOTES])
-def create_note(actor: RlcUser, title: str, note: str):
-    note_obj = Note.create(actor.org, title, note)
+def create_note(__actor: RlcUser, title: str, note: str):
+    note_obj = Note.create(__actor.org, title, note)
     note_obj.save()
 
 
 @use_case(permissions=[PERMISSION_DASHBOARD_MANAGE_NOTES])
 def update_note(
-    actor: RlcUser, note_id: int, title: str | None = None, note: str | None = None
+    __actor: RlcUser, note_id: int, title: str | None = None, note: str | None = None
 ):
-    note_obj = note_from_id(actor, note_id)
+    note_obj = note_from_id(__actor, note_id)
     note_obj.update_information(new_title=title, new_note=note)
     note_obj.save()
 
 
 @use_case(permissions=[PERMISSION_DASHBOARD_MANAGE_NOTES])
-def delete_note(actor: RlcUser, note_id: int):
-    note = note_from_id(actor, note_id)
+def delete_note(__actor: RlcUser, note_id: int):
+    note = note_from_id(__actor, note_id)
     note.delete()
