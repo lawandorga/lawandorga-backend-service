c5aca4350c78225faba1a0de5b4e89c09bf60612
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2023-03-14 20:57:08 +0100

    replace tag stats viewset

--- core/statistics/api/__init__.py
+++ core/statistics/api/__init__.py
@@ -1,5 +1,5 @@
 from .error_statistics import router as error_statistics_router
 from .general_statistics import router as general_statistics_router
+from .individual_statistics import router as individual_statistics_router
 from .org_statistics import router as rlc_statistics_router
 from .record_statistics import router as record_statistics_router
-from .individual_statistics import router as individual_statistics_router
--- core/statistics/api/individual_statistics.py
+++ core/statistics/api/individual_statistics.py
@@ -9,7 +9,9 @@ from . import schemas
 router = Router()
 
 
-@router.get("user_actions_month/", output_schema=list[schemas.OutputIndividualUserActionsMonth])
+@router.get(
+    "user_actions_month/", output_schema=list[schemas.OutputIndividualUserActionsMonth]
+)
 def query__user_actions_month(rlc_user: RlcUser):
     if connection.vendor == "sqlite":
         statement = """
@@ -42,3 +44,70 @@ def query__user_actions_month(rlc_user: RlcUser):
     data = execute_statement(statement)
     data = map(lambda x: {"email": x[0], "actions": x[1]}, data)
     return list(data)
+
+
+@router.get("tag_stats/", output_schema=schemas.OutputRecordTagStats)
+def query__tag_stats(rlc_user: RlcUser):
+    if connection.vendor == "sqlite":
+        example_data = {
+            "tags": [
+                {"tag": "Duldung", "count": 10},
+                {"tag": "Abschiebung", "count": 5},
+            ],
+            "state": [
+                {"state": "Set", "count": 10},
+                {"state": "Not-Existing", "count": 5},
+            ],
+        }
+        return example_data
+    statement = """
+        select tag, count(*) as count from (
+        select json_array_elements(value::json)::varchar as tag
+        from core_recordmultipleentry entry
+        left join core_recordmultiplefield field on entry.field_id = field.id
+        left join core_recordtemplate as template on template.id = field.template_id
+        where field.name='Tags'
+        and template.rlc_id = {}
+        ) tmp
+        group by tag
+        order by count(*) desc
+        """.format(
+        rlc_user.org_id
+    )
+    ret = {}
+    data = execute_statement(statement)
+    data = list(
+        map(
+            lambda x: {
+                "tag": x[0].replace(' "', "").replace('"', ""),
+                "count": x[1],
+            },
+            data,
+        )
+    )
+    ret["tags"] = data
+    statement = """
+        select name, count(*) as existing
+        from (
+        select case when name like '%Tags%' then 'Tags' else 'Unknown' end as name
+        from (
+        select string_agg(name, ' ') as name
+        from (
+        select record.id,
+        case when field.name = 'Tags' then 'Tags' else 'Unknown' end as name
+        from core_record record
+        left join core_recordtemplate template on record.template_id = template.id
+        left join core_recordmultiplefield field on template.id = field.template_id
+        where template.rlc_id = {}
+        ) tmp1
+        group by id
+        ) tmp2
+        ) tmp3
+        group by name
+        """.format(
+        rlc_user.org_id
+    )
+    data = execute_statement(statement)
+    data = list(map(lambda x: {"state": x[0], "count": x[1]}, data))
+    ret["state"] = data
+    return ret
