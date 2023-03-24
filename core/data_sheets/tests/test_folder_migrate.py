f645cda4a9d5a4696e2c043adba8494099d89ec6
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2023-03-24 19:54:25 +0100

    precommit

--- core/data_sheets/tests/test_folder_migrate.py
+++ core/data_sheets/tests/test_folder_migrate.py
@@ -1,6 +1,13 @@
 from core.auth.models.org_user import RlcUser
-from core.data_sheets.models.record import Record, RecordEncryptedStandardEntry, RecordEncryptionNew
-from core.data_sheets.models.template import RecordEncryptedStandardField, RecordTemplate
+from core.data_sheets.models.record import (
+    Record,
+    RecordEncryptedStandardEntry,
+    RecordEncryptionNew,
+)
+from core.data_sheets.models.template import (
+    RecordEncryptedStandardField,
+    RecordTemplate,
+)
 from core.data_sheets.use_cases.record import migrate_record_into_folder
 from core.rlc.models.org import Org
 from core.seedwork import test_helpers
@@ -11,10 +18,14 @@ def test_folder_migrate(db):
     full_user = test_helpers.create_rlc_user()
     user: RlcUser = full_user["rlc_user"]  # type: ignore
     org: Org = full_user["org"]
-    another_full_user = test_helpers.create_rlc_user(email='tester@law-orga.de', name="Mr. Tester", rlc=org)
+    another_full_user = test_helpers.create_rlc_user(
+        email="tester@law-orga.de", name="Mr. Tester", rlc=org
+    )
     another_user = another_full_user["rlc_user"]
     template = RecordTemplate.objects.create(rlc=org, name="Folder Migrate Test")
-    field = RecordEncryptedStandardField.objects.create(template=template, name="Folder Migrate Test")
+    field = RecordEncryptedStandardField.objects.create(
+        template=template, name="Folder Migrate Test"
+    )
     record = Record.objects.create(template=template, name="Folder Migrate Test")
     key = AESEncryption.generate_secure_key()
     enc = RecordEncryptionNew(user=user, record=record, key=key)
@@ -33,5 +44,8 @@ def test_folder_migrate(db):
     updated_record = Record.objects.get(uuid=record.uuid)
     assert updated_record.folder_uuid is not None
     same_entry = RecordEncryptedStandardEntry.objects.get(pk=entry.pk)
-    same_entry.decrypt(another_user, RlcUser.get_dummy_user_private_key(another_user, 'tester@law-orga.de'))
+    same_entry.decrypt(
+        another_user,
+        RlcUser.get_dummy_user_private_key(another_user, "tester@law-orga.de"),
+    )
     assert same_entry.value == SECRET
