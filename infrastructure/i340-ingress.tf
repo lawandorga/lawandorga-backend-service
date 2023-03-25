12a536905ac45baaba60d0a0fcb59feef2de0d11
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2023-03-25 15:05:28 +0100

    allow bigger file sizes to be uploaded

--- infrastructure/i340-ingress.tf
+++ infrastructure/i340-ingress.tf
@@ -9,6 +9,7 @@ resource "kubernetes_ingress_v1" "ingress" {
       "nginx.ingress.kubernetes.io/enable-cors"        = "true"
       "nginx.ingress.kubernetes.io/cors-allow-headers" = "accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with, baggage, sentry-trace"
       "nginx.ingress.kubernetes.io/cors-allow-origin"  = "https://www.law-orga.de, https://law-orga.de"
+      "nginx.ingress.kubernetes.io/proxy-body-size"    = "50m"
     }
   }
 
