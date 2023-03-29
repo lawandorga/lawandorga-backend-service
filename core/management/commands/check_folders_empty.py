853ebb49c7c5a660157dc346f0a9efd3fcabc903
danielmoessner <github@danielmoessner.de>
2023-03-29 17:53:11 +0200

    add command to check empty folders

---
+++ core/management/commands/check_folders_empty.py
@@ -0,0 +1,27 @@
+import time
+
+from django.core.management.base import BaseCommand
+
+from core.folders.models import FoldersFolder
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        t1 = time.time()
+
+        folders = list(FoldersFolder.objects.filter())
+        
+        for folder in folders:
+            contains_files = False
+            contains_record = False
+            for item in folder.items:
+                if item["repository"] == "FILE":
+                    contains_files = True
+                if item["repository"] == "RECORDS_RECORD":
+                    contains_record = True
+            if contains_files and not contains_record:
+                self.stdout.write(f"Folder '{folder.name}' contains files but no records.")
+
+        t2 = time.time()
+
+        self.stdout.write(f"Done in {t2 - t1} seconds.")
