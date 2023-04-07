f536f6cfdd2fd7ba68c169e02be6d78795305973
danielmoessner <github@danielmoessner.de>
2023-04-07 12:29:39 +0200

    fix

--- core/timeline/tests/test_usecases.py
+++
@@ -1,8 +0,0 @@
-from core.seedwork import test_helpers
-from core.timeline.use_cases import create_timeline_event
-
-
-def test_create():
-    user = test_helpers.create_raw_org_user()
-    event = create_timeline_event(user, text="test")
-    assert event.text == "test"
