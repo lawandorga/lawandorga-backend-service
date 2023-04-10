68504100f25a139ce22798631c98f4447862caea
danielmoessner <github@danielmoessner.de>
2023-04-10 19:46:02 +0200

    add another test

--- core/timeline/tests/test_usecases.py
+++ core/timeline/tests/test_usecases.py
@@ -5,6 +5,7 @@ from core.timeline.repository import (
     TimelineEventRepository,
 )
 from messagebus.impl.store import InMemoryEventStore
+from messagebus.impl.store.django import DjangoEventStore
 
 
 def test_repository():
@@ -24,6 +25,16 @@ def test_create_in_memory():
     folder = test_helpers.create_raw_folder(user)
     event1 = TimelineEvent.create(text="test", folder=folder, org_pk=user.org_id)
     r = EventStoreTimelineEventRepository(InMemoryEventStore())
+    r.save(event1, by=user)
+    event2 = r.load(event1.uuid, user, folder)
+    assert event2.text == "test"
+
+
+def test_create_in_db(db):
+    user = test_helpers.create_raw_org_user()
+    folder = test_helpers.create_raw_folder(user)
+    event1 = TimelineEvent.create(text="test", folder=folder, org_pk=user.org_id)
+    r = EventStoreTimelineEventRepository(DjangoEventStore())
     r.save(event1, by=user)
     event2 = r.load(event1.uuid, user, folder)
     assert event2.text == "test"
