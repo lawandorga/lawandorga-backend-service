efd068fa8f6b553c649d08670f6de565444cca81
danielmoessner <github@danielmoessner.de>
2023-04-23 22:29:30 +0200

    precommit

--- core/events/api/events.py
+++ core/events/api/events.py
@@ -1,5 +1,3 @@
-from typing import List
-
 import bleach
 from django.core.exceptions import ObjectDoesNotExist
 
--- core/events/models/event.py
+++ core/events/models/event.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from django.db import models
 from django.db.models import Q
 
