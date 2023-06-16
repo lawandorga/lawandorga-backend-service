349c9881dfabe524af43dbbe8d8c51965f9860f7
Daniel Mössner <github@moesibert.de>
2023-06-16 19:57:01 +0200

    update

--- core/management/commands/migrate_data.py
+++ core/management/commands/migrate_data.py
@@ -1,17 +1,8 @@
-from uuid import uuid4
-
 from django.core.management.base import BaseCommand
-from django.db import transaction
-
-from core.data_sheets.models.template import RecordField
 
 
 class Command(BaseCommand):
     help = "Migrates Data"
 
     def handle(self, *args, **options):
-        with transaction.atomic():
-            for subclass in RecordField.__subclasses__():
-                for field in subclass.objects.all():
-                    field.uuid = uuid4()
-                    field.save()
+        pass
