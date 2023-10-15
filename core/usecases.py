3d82b4fa075f178c308a76a3905d20688660ac20
Daniel Mössner <github@moesibert.de>
2023-10-15 19:50:57 +0100

    update use cases

--- core/usecases.py
+++ core/usecases.py
@@ -1,19 +1,8 @@
+from core.data_sheets.commands import COMMANDS as DATA_SHEET_COMMANDS
 from core.questionnaires.commands import COMMANDS as QUESTIONNAIRE_COMMANDS
-from core.timeline.usecases.event import create_event, delete_event, update_event
-from core.timeline.usecases.follow_up import (
-    create_follow_up,
-    delete_follow_up,
-    set_follow_up_as_done,
-    update_follow_up,
-)
+from core.timeline.commands import USECASES as TIMELINE_USECASES
 
-USECASES = {
-    "timeline/create_follow_up": create_follow_up,
-    "timeline/update_follow_up": update_follow_up,
-    "timeline/delete_follow_up": delete_follow_up,
-    "timeline/set_follow_up_as_done": set_follow_up_as_done,
-    "timeline/create_event": create_event,
-    "timeline/update_event": update_event,
-    "timeline/delete_event": delete_event,
-}
+USECASES = {}
+USECASES.update(TIMELINE_USECASES)
 USECASES.update(QUESTIONNAIRE_COMMANDS)
+USECASES.update(DATA_SHEET_COMMANDS)
