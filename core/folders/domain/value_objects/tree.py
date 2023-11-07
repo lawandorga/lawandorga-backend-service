c5b3e9239c3f0592aa7e6dce4dc5c3fe548a9692
Daniel Mössner <github@moesibert.de>
2023-11-07 21:51:04 +0100

    fix access stuff

--- core/folders/domain/value_objects/tree.py
+++ core/folders/domain/value_objects/tree.py
@@ -2,8 +2,6 @@ from typing import TYPE_CHECKING, Optional, Union
 from uuid import UUID
 
 from core.folders.domain.aggregates.folder import Folder
-from core.folders.domain.value_objects.folder_key import EncryptedFolderKeyOfUser
-from core.folders.domain.value_objects.parent_key import ParentKey
 
 from seedwork.types import JsonDict
 
@@ -26,25 +24,22 @@ class TreeAccess:
     def get_owners_with_access(self, folder: Folder, source="direct"):
         access = []
         for key in folder.keys:
-            if isinstance(key, EncryptedFolderKeyOfUser):
-                user = self.__users_dict[key.owner_uuid]
-                access.append(
-                    {
-                        "name": user.name,
-                        "uuid": str(key.owner_uuid),
-                        "is_valid": key.is_valid,
-                        "source": source,
-                        "actions": self.get_actions(folder, key, source),
-                    }
-                )
-                continue
-            if isinstance(key, ParentKey):
-                if not folder.stop_inherit:
-                    assert folder.parent_uuid is not None
-                    f = self.__folders_dict[folder.parent_uuid]
-                    access += self.get_owners_with_access(f, "parent")
-                continue
-            raise ValueError("Unknown type of key.")
+            user = self.__users_dict[key.owner_uuid]
+            access.append(
+                {
+                    "name": user.name,
+                    "uuid": str(key.owner_uuid),
+                    "is_valid": key.is_valid,
+                    "source": source,
+                    "actions": self.get_actions(folder, key, source),
+                }
+            )
+
+        if not folder.stop_inherit and folder.parent_uuid is not None:
+            assert folder.parent_uuid is not None
+            f = self.__folders_dict[folder.parent_uuid]
+            access += self.get_owners_with_access(f, "parent")
+
         return access
 
     def get_actions(self, folder, key, source):
