5f791f7080be151638342a8f732c1181ed0d3d8f
Daniel Mössner <github@moesibert.de>
2024-01-15 19:05:45 +0100

    typing fixes

--- core/auth/views/mfa.py
+++ core/auth/views/mfa.py
@@ -1,12 +1,13 @@
-from django import views
 from django.conf import settings
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import RedirectURLMixin  # type: ignore
-from django.http import Http404, HttpResponseRedirect
+from django.http import Http404, HttpRequest, HttpResponseRedirect
 from django.urls import reverse_lazy
+from django.views import generic
 
 from core.auth.forms.mfa import CheckMfaForm, MfaAuthenticationForm, SetupMfaForm
+from core.auth.models.user import UserProfile
 from core.auth.use_cases.mfa import (
     create_mfa_secret,
     delete_mfa_secret,
@@ -15,12 +16,21 @@ from core.auth.use_cases.mfa import (
 from core.auth.views.user import strip_scheme
 
 
-class MfaStatusView(LoginRequiredMixin, views.generic.TemplateView):
+class GetUserMixin:
+    request: HttpRequest
+
+    def get_user(self) -> "UserProfile":
+        if not hasattr(self.request, "user"):
+            raise Http404()
+        return self.request.user  # type: ignore
+
+
+class MfaStatusView(LoginRequiredMixin, GetUserMixin, generic.TemplateView):
     template_name = "mfa/status.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        user = self.request.user
+        user = self.get_user()
         if not hasattr(user, "rlc_user"):
             context["mfa_impossible"] = True
             return context
@@ -34,41 +44,41 @@ class MfaStatusView(LoginRequiredMixin, views.generic.TemplateView):
         return context
 
 
-class MfaSetupView(LoginRequiredMixin, views.generic.FormView):
+class MfaSetupView(LoginRequiredMixin, GetUserMixin, generic.FormView):
     template_name = "mfa/setup.html"
     form_class = SetupMfaForm
 
     def get_form(self, form_class=None):
-        return SetupMfaForm(self.request.user, **self.get_form_kwargs())
+        return SetupMfaForm(self.get_user(), **self.get_form_kwargs())
 
     def form_valid(self, form):
-        create_mfa_secret(self.request.user.rlc_user)
+        create_mfa_secret(self.get_user().rlc_user)
         url = reverse_lazy("mfa_enable")
         return HttpResponseRedirect(url)
 
 
-class MfaEnableView(LoginRequiredMixin, views.generic.FormView):
+class MfaEnableView(LoginRequiredMixin, GetUserMixin, generic.FormView):
     template_name = "mfa/enable.html"
     success_url = reverse_lazy("mfa_status")
 
     def get_context_data(self, **kwargs):
         if not hasattr(self.request.user, "rlc_user") or not hasattr(
-            self.request.user.rlc_user, "mfa_secret"
+            self.get_user().rlc_user, "mfa_secret"
         ):
             raise Http404()
         context = super().get_context_data(**kwargs)
-        context["object"] = self.request.user.rlc_user.mfa_secret
+        context["object"] = self.get_user().rlc_user.mfa_secret
         return context
 
     def get_form(self, form_class=None):
-        return CheckMfaForm(self.request.user, **self.get_form_kwargs())
+        return CheckMfaForm(self.get_user(), **self.get_form_kwargs())
 
     def form_valid(self, form):
-        enable_mfa_secret(self.request.user.rlc_user)
+        enable_mfa_secret(self.get_user().rlc_user)
         return HttpResponseRedirect(self.get_success_url())
 
 
-class MfaDisableView(LoginRequiredMixin, views.generic.FormView):
+class MfaDisableView(LoginRequiredMixin, GetUserMixin, generic.FormView):
     template_name = "mfa/disable.html"
     success_url = reverse_lazy("mfa_status")
 
@@ -78,14 +88,14 @@ class MfaDisableView(LoginRequiredMixin, views.generic.FormView):
         return super().get_context_data(**kwargs)
 
     def get_form(self, form_class=None):
-        return CheckMfaForm(self.request.user, **self.get_form_kwargs())
+        return CheckMfaForm(self.get_user(), **self.get_form_kwargs())
 
     def form_valid(self, form):
-        delete_mfa_secret(self.request.user.rlc_user)
+        delete_mfa_secret(self.get_user().rlc_user)
         return HttpResponseRedirect(self.get_success_url())
 
 
-class MfaLoginView(RedirectURLMixin, views.generic.FormView):
+class MfaLoginView(RedirectURLMixin, generic.FormView):
     template_name = "mfa/login.html"
     form_class = MfaAuthenticationForm
     next_page = settings.MAIN_FRONTEND_URL
