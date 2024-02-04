33b0befd3a268afc39f95dbcb95f56620cb90435
Daniel Mössner <github@moesibert.de>
2024-02-04 20:56:05 +0100

    update

--- core/rlc/tests/test_org.py
+++ core/rlc/tests/test_org.py
@@ -1,5 +1,3 @@
-import json
-
 import pytest
 from django.test import Client
 
@@ -7,6 +5,7 @@ from core.models import Org
 from core.permissions.static import PERMISSION_ADMIN_MANAGE_USERS
 from core.rlc.models import ExternalLink
 from core.rlc.use_cases.link import create_link, delete_link
+from core.rlc.use_cases.org import accept_member_to_org
 from core.seedwork import test_helpers as data
 
 
@@ -56,9 +55,6 @@ def test_member_accept(user, db):
     another_user = data.create_org_user(
         rlc=user["rlc_user"].org, email="another@law-orga.de"
     )
-    response = c.post(
-        "/api/org/accept_member/",
-        json.dumps({"user": another_user["rlc_user"].id}),
-        content_type="application/json",
-    )
-    assert response.status_code == 200
+    accept_member_to_org(user["rlc_user"], another_user["rlc_user"].id)
+    another_user["rlc_user"].refresh_from_db()
+    assert another_user["rlc_user"].accepted
