5ef8f6f2f84d5e0f7642f492de2d3de8525b46f7
LeandraH <leandravhahn@gmail.com>
2024-03-07 13:07:54 +0100

    Add marking emails as pinned

--- core/mail_imports/use_cases/mail_import.py
+++ core/mail_imports/use_cases/mail_import.py
@@ -6,4 +6,11 @@ from core.seedwork.use_case_layer import use_case
 
 @use_case
 def mark_emails_as_read(__actor: OrgUser, email_uuids: list[UUID]):
+    print("marking emails", email_uuids, "as read")
+    pass
+
+
+@use_case
+def mark_email_as_pinned(__actor: OrgUser, email_uuid: UUID):
+    print("toggling email", email_uuid, "as pinned")
     pass
--- core/mail_imports/usecases.py
+++ core/mail_imports/usecases.py
@@ -1,5 +1,9 @@
-from core.mail_imports.use_cases.mail_import import mark_emails_as_read
+from core.mail_imports.use_cases.mail_import import (
+    mark_email_as_pinned,
+    mark_emails_as_read,
+)
 
 USECASES = {
     "mail_imports/mark_emails_as_read": mark_emails_as_read,
+    "mail_imports/mark_email_as_pinned": mark_email_as_pinned,
 }
