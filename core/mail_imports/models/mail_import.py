5bc8563bd79810fc1188b4825e5238f0d365d53d
Daniel Mössner <github@moesibert.de>
2024-03-14 23:32:10 +0100

    assign emails to folders

--- core/mail_imports/models/mail_import.py
+++ core/mail_imports/models/mail_import.py
@@ -3,7 +3,7 @@ from email import message_from_bytes
 from email.message import Message
 from imaplib import IMAP4_SSL
 from typing import Any, Sequence
-from uuid import uuid4
+from uuid import UUID, uuid4
 
 from django.conf import settings
 from django.db import models
@@ -19,16 +19,21 @@ class MailImport(models.Model):
 
     uuid = models.UUIDField(db_index=True, default=uuid4, unique=True, editable=False)
     sender = models.CharField(max_length=255, blank=False)
+    cc = models.CharField(max_length=255, blank=True)
     bcc = models.CharField(max_length=255, blank=True)
     subject = models.CharField(max_length=255, blank=True)
     content = models.TextField(max_length=255, blank=True)
     sending_datetime = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
     is_pinned = models.BooleanField(default=False)
+    folder_uuid = models.UUIDField(db_index=True)
 
     class Meta:
         verbose_name = "MI_MailImport"
         verbose_name_plural = "MI_MailImports"
+
+    def __str__(self) -> str:
+        return f"mailImport: {self.folder_uuid};"
 
 
 class RawEmail(BaseModel):
@@ -40,10 +45,24 @@ class ValidatedEmail(BaseModel):
     num: str
     sender: str
     to: str
-    bcc: str | None
+    cc: str
+    bcc: str
     date: str
     subject: str
     content: str
+
+    @property
+    def email_addresses(self) -> list[str]:
+        e1 = self.to.split(",")
+        e2 = self.bcc.split(",")
+        e3 = self.cc.split(",")
+        e4 = e1 + e2 + e3
+        e5 = [e.strip() for e in e4]
+        return e5
+
+
+class AssignedEmail(ValidatedEmail):
+    folder_uuid: UUID
 
 
 class ErrorEmail(BaseModel):
@@ -82,7 +101,13 @@ class MailInbox:
     def mark_emails_as_error(self, emails: list[ErrorEmail]):
         for email in emails:
             logger.warning(f"Error in email {email.num}: {email.error}")
-            self.mailbox.store(email.num, "+FLAGS", "\\Flagged")
+            self.mailbox.copy(email.num, "Errors")
+            self.mailbox.store(email.num, "+FLAGS", "\\Deleted")
+    
+    def mark_emails_as_not_assignable(self, emails: Sequence[ValidatedEmail | ErrorEmail | RawEmail]):
+        for email in emails:
+            self.mailbox.copy(email.num, "Unassigned")
+            self.mailbox.store(email.num, "+FLAGS", "\\Deleted")
 
 
 def validate_emails(raw_emails: list[RawEmail]) -> list[ErrorEmail | ValidatedEmail]:
@@ -98,11 +123,30 @@ def validate_emails(raw_emails: list[RawEmail]) -> list[ErrorEmail | ValidatedEm
     return validated_emails
 
 
+def assign_emails(emails: list[ValidatedEmail]) -> list[AssignedEmail]:
+    assigned_emails: list[AssignedEmail] = []
+    for email in emails:
+        for address in email.email_addresses:
+            left = address.split("@")[0]
+            try:
+                folder_uuid = UUID(left)
+                assigned_emails.append(AssignedEmail(folder_uuid=folder_uuid, **email.model_dump()))
+            except Exception:
+                continue
+    return assigned_emails
+
+
+def get_unassigned_emails(validated: list[ValidatedEmail], assigned: list[AssignedEmail]) -> list[ValidatedEmail]:
+    assigned_nums = [email.num for email in assigned]
+    return [email for email in validated if email.num not in assigned_nums]
+
+
 def get_email_info(message: Message):
     return {
         "sender": message.get("From"),
         "to": message.get("To"),
-        "bcc": message.get("BCC"),
+        "bcc": message.get("BCC", ""),
+        "cc": message.get("CC", ""),
         "date": message.get("Date"),
         "subject": message.get("Subject"),
         "content": get_content_from_email(message),
@@ -117,7 +161,7 @@ def get_content_from_email(message: Message):
     return content
 
 
-def save_emails(emails: list[ValidatedEmail]):
+def save_emails(emails: list[AssignedEmail]):
     for email in emails:
         obj = MailImport(
             sender=email.sender,
@@ -125,5 +169,6 @@ def save_emails(emails: list[ValidatedEmail]):
             subject=email.subject,
             content=email.content,
             sending_datetime=email.date,
+            folder_uuid=email.folder_uuid,
         )
         obj.save()
--- core/management/commands/importmails.py
+++ core/management/commands/importmails.py
@@ -1,9 +1,12 @@
 from django.core.management.base import BaseCommand
 
 from core.mail_imports.models.mail_import import (
+    AssignedEmail,
     ErrorEmail,
     MailInbox,
     ValidatedEmail,
+    assign_emails,
+    get_unassigned_emails,
     save_emails,
     validate_emails,
 )
@@ -17,16 +20,26 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with MailInbox() as mail_inbox:
             raw_emails = mail_inbox.get_raw_emails()
-            self.stdout.write(f"Found {len(raw_emails)} emails")
+            self.stdout.write(f"|-- {len(raw_emails)} emails were found")
+
             emails = validate_emails(raw_emails)
-            self.stdout.write(f"Validated {len(emails)} emails")
+            self.stdout.write(f"|-- {len(emails)} emails got checked")
+            
             validated: list[ValidatedEmail] = list_filter(emails, lambda e: isinstance(e, ValidatedEmail))  # type: ignore
-            self.stdout.write(f"{len(validated)} are valid")
+            self.stdout.write(f"  |-- {len(validated)} are valid")
+            assigned = assign_emails(validated)
+            self.stdout.write(f"    |-- {len(assigned)} emails are assigned")
+            save_emails(assigned)
+            self.stdout.write(f"      |-- {len(assigned)} emails got saved in django")
+            mail_inbox.delete_emails(assigned)
+            self.stdout.write(f"      |-- {len(assigned)} emails got deleted from inbox")
+            
+            unassigned = get_unassigned_emails(validated, assigned)
+            self.stdout.write(f"    |-- {len(unassigned)} emails are unassigned")
+            mail_inbox.mark_emails_as_not_assignable(unassigned)
+            self.stdout.write(f"      |-- {len(unassigned)} emails got marked as unassigned")
+
             errors: list[ErrorEmail] = list_filter(emails, lambda e: isinstance(e, ErrorEmail))  # type: ignore
-            self.stdout.write(f"{len(errors)} are invalid")
-            save_emails(validated)
-            self.stdout.write(f"Saved {len(validated)} emails")
-            mail_inbox.delete_emails(validated)
-            self.stdout.write(f"Deleted {len(validated)} emails")
+            self.stdout.write(f"  |-- {len(errors)} are invalid")
             mail_inbox.mark_emails_as_error(errors)
-            self.stdout.write(f"Marked {len(errors)} emails as error")
+            self.stdout.write(f"    |-- Marked {len(errors)} emails as error")
