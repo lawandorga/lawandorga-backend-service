9905cd68ded263298dfa402e6be60578dbe6f0e7
JoeyStk <johannes.stock@yahoo.de>
2024-04-02 09:41:02 +0200

    Add test for mark as pinned usecase

--- core/mail_imports/tests/test_model.py
+++ core/mail_imports/tests/test_model.py
@@ -13,3 +13,14 @@ def test_mark_as_read_works():
     )
     mail.mark_as_read()
     assert mail.is_read
+
+def test_mark_as_pinned_works():
+    folder_uuid = uuid4()
+    mail = MailImport.create(
+        sender="test@law-orga.de",
+        content="Test Mail",
+        folder_uuid=folder_uuid,
+        subject="Test Mail",
+    )
+    mail.mark_as_pinned()
+    assert mail.is_pinned
