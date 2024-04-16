bc5d8139343ef218f390bafd6f2324fd26e871e9
JoeyStk <johannes.stock@yahoo.de>
2024-04-16 10:07:47 +0200

    Add test to check if mail can be unpinned

--- core/mail_imports/tests/test_model.py
+++ core/mail_imports/tests/test_model.py
@@ -46,6 +46,23 @@ def test_mark_as_pinned_works():
     assert mail.is_pinned
 
 
+def test_mark_as_unpinned_works():
+    folder_uuid = uuid4()
+    mail = MailImport.create(
+        sender="test@law-orga.de",
+        to="recieve@law-orag.de",
+        content="Test Mail",
+        folder_uuid=folder_uuid,
+        subject="Test Mail",
+        org_id=0,
+    )
+    assert not mail.is_pinned
+    mail.toggle_pinned()
+    assert mail.is_pinned
+    mail.toggle_pinned()
+    assert not mail.is_pinned
+
+
 def test_encryption(db):
     u = test_helpers.create_org_user()
     user = u["rlc_user"]
