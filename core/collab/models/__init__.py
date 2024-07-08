bb3657464eb6cc77eb1947a8e4031a8f059ca370
Daniel Mössner <github@moesibert.de>
2024-07-08 12:54:43 +0200

    fix mypy

--- core/collab/models/__init__.py
+++ core/collab/models/__init__.py
@@ -1,4 +1,4 @@
-from .collab import *
+from .collab import Collab, Footer, Letterhead
 from .deprecated_collab_document import *
 from .deprecated_collab_permission import *
 from .deprecated_permission_for_collab_document import *
