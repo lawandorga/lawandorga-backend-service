54fb6c68b400684ed4879a21e92ca2af69cdb3af
JoeyStk <johannes.stock@yahoo.de>
2024-09-03 13:42:08 +0200

    Fix precommit

--- core/mail_imports/admin.py
+++ core/mail_imports/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from core.mail_imports.models.mail_import import MailImport, MailAttachement
+from core.mail_imports.models.mail_import import MailAttachement, MailImport
 
 admin.site.register(MailImport)
 admin.site.register(MailAttachement)
