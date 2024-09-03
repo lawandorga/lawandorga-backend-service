d4d74b39eb089f7850eb4253e96788ccaf158098
JoeyStk <johannes.stock@yahoo.de>
2024-09-03 13:40:47 +0200

    Add mail attachment to admin pannel

--- core/mail_imports/admin.py
+++ core/mail_imports/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from core.mail_imports.models.mail_import import MailImport
+from core.mail_imports.models.mail_import import MailImport, MailAttachement
 
 admin.site.register(MailImport)
+admin.site.register(MailAttachement)
