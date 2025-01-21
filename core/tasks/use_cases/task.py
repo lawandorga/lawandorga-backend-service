b030ee351d66a0cb37bbe3af8a280c175c13303d
JoeyStk <johannes.stock@yahoo.de>
2025-01-21 16:34:41 +0100

    Add update usecase

--- core/tasks/use_cases/task.py
+++ core/tasks/use_cases/task.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from uuid import UUID
+from typing import Optional
 
 from core.auth.models.org_user import OrgUser
 from core.seedwork.use_case_layer import use_case
@@ -27,6 +28,36 @@ def create_task(
 ):
     Task.create(creator, assignee, title, description, page_url, updated_at, deadline)
 
+@use_case
+def update_task(
+    task_id: UUID,
+    updater: OrgUser,
+    title: Optional[str] = None,
+    description: Optional[str] = None,
+    page_url: Optional[str] = None,
+    assignee: Optional[OrgUser] = None,
+    is_done: Optional[bool] = None,
+    deadline: Optional[datetime] = None,
+):
+    task = Task.objects.get(uuid=task_id)
+
+    if task.creator != updater and task.assignee != updater:
+        raise PermissionError("You are not allowed to update this task.")
+
+    if title is not None:
+        task.title = title
+    if description is not None:
+        task.description = description
+    if page_url is not None:
+        task.page_url = page_url
+    if assignee is not None:
+        task.assignee = assignee
+    if is_done is not None:
+        task.is_done = is_done
+    if deadline is not None:
+        task.deadline = deadline
+
+    task.save()
 
 @use_case
 def delete_task(__actor: OrgUser, task_uuid):
--- core/tasks/usecases.py
+++ core/tasks/usecases.py
@@ -1,7 +1,13 @@
-from core.tasks.use_cases.task import create_task, delete_task, mark_tasks_as_done
+from core.tasks.use_cases.task import (
+    create_task,
+    delete_task,
+    mark_tasks_as_done,
+    update_task,
+)
 
 USECASES = {
     "tasks/mark_tasks_as_done": mark_tasks_as_done,
     "tasks/create_task": create_task,
+    "tasks/update_task": update_task,
     "tasks/delete_task": delete_task,
 }
