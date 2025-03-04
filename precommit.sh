1713ae46242bd76c65e56485cfe032edbe0368c9
danielmoessner <github@danielmoessner.de>
2025-03-04 18:00:13 +0100

    remove flake8

--- precommit.sh
+++ precommit.sh
@@ -7,9 +7,7 @@ printf "\nisort:\n" && pipenv run isort . --profile black
 printf "\n\n"
 printf "black:\n" && pipenv run black .
 printf "\n\n"
-printf "flake8:\n"  && pipenv run flake8 .
-printf "\n\n"
-printf "ruff:\n"  && pipenv run ruff check .
+printf "ruff:\n"  && pipenv run ruff check . --fix
 printf "\n\n"
 printf "mypy:\n"  && pipenv run mypy .
 printf "\n\n"
