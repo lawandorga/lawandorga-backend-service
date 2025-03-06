d394a53b754a8e5041fdeb1fbc9d43185ac27bfb
danielmoessner <github@danielmoessner.de>
2025-03-06 23:21:58 +0100

    fiux name

--- core/auth/models/session.py
+++ core/auth/models/session.py
@@ -8,8 +8,8 @@ class CustomSession(AbstractBaseSession):
     user_id = models.IntegerField(null=True, db_index=True)
 
     class Meta:
-        verbose_name = "AUTH_CustomSession"
-        verbose_name_plural = "AUTH_CustomSessions"
+        verbose_name = "AUT_CustomSession"
+        verbose_name_plural = "AUT_CustomSessions"
 
     @classmethod
     def get_session_store_class(cls):
