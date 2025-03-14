908cfd9c4dcceba8b76dc2024d00f7065832009d
danielmoessner <github@danielmoessner.de>
2025-03-14 20:06:21 +0100

    add some tests

--- core/folders/tests/test_use_cases/test_folder.py
+++ core/folders/tests/test_use_cases/test_folder.py
@@ -1,6 +1,9 @@
+
 from core.folders.infrastructure.folder_repository import DjangoFolderRepository
-from core.folders.use_cases.folder import create_folder, rename_folder
+from core.folders.models import FOL_Folder
+from core.folders.use_cases.folder import create_folder, delete_folder, rename_folder
 from core.seedwork import test_helpers
+
 
 
 def test_create_folder(db):
@@ -15,3 +18,10 @@ def test_rename_folder(db):
     rename_folder(org_user, "New Folder", folder.uuid)
     folder = DjangoFolderRepository().retrieve(org_user.org_id, folder.uuid)
     assert folder.name == "New Folder"
+
+
+def test_delete_folder(db):
+    org_user = test_helpers.create_org_user()["org_user"]
+    folder = test_helpers.create_folder("Test Folder", org_user)["folder"]
+    delete_folder(org_user, folder.uuid)
+    assert FOL_Folder.objects.get(uuid=folder.uuid).deleted
