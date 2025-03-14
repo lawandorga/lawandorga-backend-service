e09afcc14c3b9599dccdedf800580e2ae503364e
danielmoessner <github@danielmoessner.de>
2025-03-14 20:03:12 +0100

    add tests

---
+++ core/folders/tests/test_use_cases/test_folder.py
@@ -0,0 +1,17 @@
+from core.folders.infrastructure.folder_repository import DjangoFolderRepository
+from core.folders.use_cases.folder import create_folder, rename_folder
+from core.seedwork import test_helpers
+
+
+def test_create_folder(db):
+    org_user = test_helpers.create_org_user()["org_user"]
+    create_folder(org_user, "Test Folder", None)
+    assert org_user.org.folders_folders.count() == 1
+
+
+def test_rename_folder(db):
+    org_user = test_helpers.create_org_user()["org_user"]
+    folder = test_helpers.create_folder("Test Folder", org_user)["folder"]
+    rename_folder(org_user, "New Folder", folder.uuid)
+    folder = DjangoFolderRepository().retrieve(org_user.org_id, folder.uuid)
+    assert folder.name == "New Folder"
