bb0b763cd316de734fd5663e637607aa8ad8e1a8
danielmoessner <github@danielmoessner.de>
2026-01-29 21:20:57 +0100

    fix: go back to full certificate check

--- config/settings/production.py
+++ config/settings/production.py
@@ -56,7 +56,10 @@ DATABASES = {
         "PORT": env.int("DB_PORT"),
         "TEST": {"NAME": "test_{}".format(RUNTIME)},
         "OPTIONS": {
-            "sslmode": "require",
+            "sslmode": "verify-full",
+            "sslrootcert": os.path.join(
+                BASE_DIR, "static/dist/lawandorga-backend-service.cer"
+            ),
         },
     }
 }
