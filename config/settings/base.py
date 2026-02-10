b1e0e95cd3fedf84fcae11ac8dabe27166261729
JoeyStk <johannes.stock@yahoo.de>
2026-02-10 12:07:10 +0100

    Fix pre-commit

--- config/settings/base.py
+++ config/settings/base.py
@@ -179,7 +179,10 @@ OIDC_IDTOKEN_INCLUDE_CLAIMS = True
 
 # mail errors to the admins
 # https://docs.djangoproject.com/en/3.2/ref/settings/#admins
-ADMINS = [("Daniel Mössner", "it@law-orga.de"), ("Johannes Stock", "johannes.stock@tuerantuer.org")]
+ADMINS = [
+    ("Daniel Mössner", "it@law-orga.de"),
+    ("Johannes Stock", "johannes.stock@tuerantuer.org"),
+]
 
 # TinyMCE
 # https://django-tinymce.readthedocs.io/en/latest/installation.html#configuration
