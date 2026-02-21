3d87c593810895bb3c27f5adc46fc1754fb848ca
danielmoessner <github@danielmoessner.de>
2026-02-21 15:56:16 +0100

    feat: improve logged path page speed

--- core/other/admin.py
+++ core/other/admin.py
@@ -6,5 +6,8 @@ from .models import LoggedPath
 class LoggedPathAdmin(admin.ModelAdmin):
     search_fields = ("path", "user__email", "status")
 
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related("user")
+
 
 admin.site.register(LoggedPath, LoggedPathAdmin)
