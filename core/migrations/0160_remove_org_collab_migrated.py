e6f29efbd647a11e3657780acdefcaee82365c92
JoeyStk <johannes.stock@yahoo.de>
2026-04-11 10:44:29 +0200

    Fix migration

--- core/migrations/0160_remove_org_collab_migrated.py
+++ core/migrations/0160_remove_org_collab_migrated.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0157_task_tags"),
+        ("core", "0159_calendarevent_calendareventattachment_and_more"),
     ]
 
     operations = [
