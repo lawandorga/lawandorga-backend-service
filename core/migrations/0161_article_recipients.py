4b8209908a4430de6190f55dda206061d5e7f454
JoeyStk <johannes.stock@yahoo.de>
2026-04-24 18:08:47 +0200

    Fix migration order

--- core/migrations/0161_article_recipients.py
+++ core/migrations/0161_article_recipients.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0158_remove_eventsevent_is_global"),
+        ("core", "0160_remove_org_collab_migrated"),
     ]
 
     operations = [
