253a6c4d7e59cc178150b6e71c4443bf209f8e35
danielmoessner <github@danielmoessner.de>
2026-07-17 15:38:27 +0200

    feat: update record timestamps on message create or delete

--- core/messages/use_cases/message.py
+++ core/messages/use_cases/message.py
@@ -1,10 +1,19 @@
 from uuid import UUID
 
 from core.auth.models import OrgUser
+from core.data_sheets.use_cases.finders import find_record_from_folder_uuid
 from core.folders.use_cases.finders import folder_from_uuid
 from core.messages.models import EncryptedRecordMessage
 from core.messages.use_cases.finders import get_message_by_uuid
 from core.seedwork.use_case_layer import use_case
+
+
+def update_record_updated_at(__actor: OrgUser, folder_uuid: UUID):
+    record = find_record_from_folder_uuid(__actor, folder_uuid)
+    if not record:
+        return
+    record.update_timestamps()
+    record.save()
 
 
 @use_case
@@ -16,9 +25,12 @@ def create_a_message(__actor: OrgUser, message: str, folder_uuid: UUID):
     )
     m.encrypt(user=__actor)
     m.save()
+    update_record_updated_at(__actor, folder.uuid)
 
 
 @use_case
 def delete_message(__actor: OrgUser, message_id: int):
     message = get_message_by_uuid(__actor, message_id)
+    folder_uuid = message.folder_uuid
     message.delete()
+    update_record_updated_at(__actor, folder_uuid)
